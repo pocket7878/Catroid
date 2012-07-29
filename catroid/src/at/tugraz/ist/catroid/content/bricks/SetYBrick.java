ed43fdbb86f74ac1f74ed04e559d62fa7c2f2361
SimgeSezgin <simgeszgn@gmail.com>
2012-07-29 19:35:57 +0300

    imported WallpaperCostume in these classes

--- catroid/src/at/tugraz/ist/catroid/content/bricks/SetYBrick.java
+++ catroid/src/at/tugraz/ist/catroid/content/bricks/SetYBrick.java
@@ -35,6 +35,7 @@ import android.widget.Toast;
 import at.tugraz.ist.catroid.R;
 import at.tugraz.ist.catroid.content.Sprite;
 import at.tugraz.ist.catroid.utils.Utils;
+import at.tugraz.ist.catroid.livewallpaper.WallpaperCostume;
 
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
